19d3b952912f7428ccdb35efaf1dd13f72e8e88c
Mickle Kozlov <33799845+Borntowarn@users.noreply.github.com>
2022-04-22 14:41:01 +0300

    Добавил метод поиска CMC_Id. Завернул все в класс

--- Bot/CMC_interact.py
+++ Bot/CMC_interact.py
@@ -1,37 +1,60 @@
-"""
-Взаимодействие с API сайта CoinMarketCap
-"""
-
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 
+class CMC_API:
+    token = '9273b9b2-6158-428f-afd4-379f3c3992de'
+    url = 'https://pro-api.coinmarketcap.com'
+    urls = {
+      'map': f'{url}/v1/cryptocurrency/map',
+      'latestData': f'{url}/v2/cryptocurrency/quotes/latest',
+      'newListings': f'{url}/v1/cryptocurrency/listings/new',
+      'airdrops': f'{url}/v1/cryptocurrency/airdrops',
+      'fiat': f'{url}/v1/fiat/map'
+      }
+    headers = {
+      'Accepts': 'application/json',
+      'X-CMC_PRO_API_KEY': '9273b9b2-6158-428f-afd4-379f3c3992de',
+    }
 
-token = '9273b9b2-6158-428f-afd4-379f3c3992de'
-url = 'https://pro-api.coinmarketcap.com'
 
-urls = {
-  'map': f'{url}/v1/cryptocurrency/map',
-  'latestData': f'{url}/v2/cryptocurrency/quotes/latest',
-  'newListings': f'{url}/v1/cryptocurrency/listings/new',
-  'airdrops': f'{url}/v1/cryptocurrency/airdrops'
-  }
+    def __init__(self) -> None:
+        self.session = Session()
+        self.session.headers.update(self.headers)
 
-parameters = {
-  'slug': 'bitcoin'
-}
 
-headers = {
-  'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': '9273b9b2-6158-428f-afd4-379f3c3992de',
-}
+    def get_stat(self, symbols='', convert='USD'):
+        parameters = {
+          'symbol': symbols,
+          'convert_id': self.get_CMC_id(convert)
+        }
+        try:
+          response = self.session.get(self.urls['latestData'], params=parameters)
+          data = json.loads(response.text)
+          return(data['data'])
+        except (ConnectionError, Timeout, TooManyRedirects) as e:
+          return(e)
+      
+    
+    def get_CMC_id(self, symbols):
+        """
+        Function helper
 
-session = Session()
-session.headers.update(headers)
+        Args:
+            symbols (str): Comma-separated list of symbols of crypto(currencies)
 
-try:
-  response = session.get(urls['latestData'], params=parameters)
-  data = json.loads(response.text)
-  print(data['data'])
-except (ConnectionError, Timeout, TooManyRedirects) as e:
-  print(e)
+        Returns:
+            str: symbols (str): Comma-separated list of CMC_ID of crypto(currencies)
+        """
+        symbols = [i.strip().upper() for i in symbols.split(',')]
+        try:
+          response_fiat = self.session.get(self.urls['fiat'])
+          response_crypto = self.session.get(self.urls['map'])
+          data = json.loads(response_fiat.text)['data']
+          crypto_data = json.loads(response_crypto.text)['data']
+          
+          data.extend(crypto_data)
+          ids = [str(item['id']) for item in data if item['symbol'] in symbols]
+          return ','.join(ids)
+        except (ConnectionError, Timeout, TooManyRedirects) as e:
+          return(e)
